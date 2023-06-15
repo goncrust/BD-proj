a10a01d4cec7120c5573cfa6cd2619911bf268eb
João Gouveia <jjtucki4@gmail.com>
2023-06-15 18:31:38 +0100

    Several improvements

--- E3/web/app.py
+++ E3/web/app.py
@@ -7,7 +7,7 @@ import os
 import re
 
 email_regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
-float_regex = re.compile(r'\d{1,}[.,]\d{1,}')
+float_regex = re.compile(r'\d+[.,]\d+')
 
 DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://db:db@postgres/db")
 pool = ConnectionPool(conninfo=DATABASE_URL)
@@ -41,7 +41,7 @@ def home():
 def gerir_clientes():
     with pool.connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT cust_no, name FROM customer WHERE NOT email = 'deleted';")
+            cursor.execute("SELECT cust_no, name FROM customer WHERE NOT email LIKE '%deleted';")
             parser = lambda el : {"id": el[0], "name": el[1]}
             clients = list(map(parser, cursor.fetchall()))
     return render_template("gerir-clientes.html", clients=clients)
@@ -50,22 +50,23 @@ def gerir_clientes():
 def gerir_clientes_delete(cust_no):
     with pool.connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("UPDATE customer SET name = 'deleted', email = 'deleted', phone = NULL, ADDRESS = NULL WHERE cust_no = %(cust_no)s;", {"cust_no": cust_no})
+            email = cust_no + "deleted"
+            cursor.execute("UPDATE customer SET name = 'deleted', email = %(email)s, phone = NULL, ADDRESS = NULL WHERE cust_no = %(cust_no)s;", {"cust_no": cust_no, "email": email})
     return redirect("/gerir-clientes")
 
 @app.route("/gerir-clientes/add", methods=("POST",))
 def gerir_clientes_add():
     name = request.form["name"]
     email = request.form["email"]
-    phone = request.form["phone"] if request.form["phone"] else "NULL"
-    address = request.form["address"] if request.form["address"] else "NULL"
+    phone = request.form["phone"] if request.form["phone"] else None
+    address = request.form["address"] if request.form["address"] else None
     error = ""
     
     if not name:
         error = "Nome inválido"
     elif not email or not re.fullmatch(email_regex, email):
         error = "Email inválido"
-    elif phone != "NULL" and not phone.isnumeric():
+    elif phone != None and not phone.isnumeric():
         error = "Telemóvel inválido"
 
     if error:
@@ -77,6 +78,7 @@ def gerir_clientes_add():
                 existing_email = cursor.execute("SELECT email FROM customer WHERE email = %(email)s;", {"email": email}).fetchone()
                 if existing_email:
                     flash("O email já existe")
+                    cursor.execute("ROLLBACK;")
                     return redirect("/gerir-clientes")
 
                 cust_no = cursor.execute("SELECT MAX(cust_no) FROM customer;").fetchone()[0]
@@ -113,6 +115,18 @@ def gerir_produtos_delete(sku):
             cursor.execute("DELETE FROM contains WHERE sku = %(sku)s;", {"sku": sku})
             cursor.execute(
                 """
+                DELETE FROM process
+                WHERE NOT order_no IN
+                (SELECT DISTINCT order_no FROM contains)
+                """)
+            cursor.execute(
+                """
+                DELETE FROM pay
+                WHERE NOT order_no IN
+                (SELECT DISTINCT order_no FROM contains)
+                """)
+            cursor.execute(
+                """
                 DELETE FROM orders
                 WHERE NOT order_no IN
                 (SELECT DISTINCT order_no FROM contains)
@@ -135,16 +149,18 @@ def gerir_produtos_edit(sku):
         return render_template("editar-produto.html", product=product)
     else:
         name = request.form["name"]
-        description = request.form["description"] if request.form["description"] else "NULL"
+        description = request.form["description"] if request.form["description"] else None
         price = request.form["price"]
-        ean = request.form["ean"] if request.form["ean"] else "NULL"
+        ean = request.form["ean"] if request.form["ean"] else None
         error = ""
         
         if not name:
             error = "Nome inválido"
         elif not price or (not price.isnumeric() and not re.fullmatch(float_regex, price)):
             error = "Preço inválido"
-        elif ean != "NULL" and not ean.isnumeric():
+        elif float(price) < 0:
+            error = "Preço inválido"
+        elif ean != None and not ean.isnumeric():
             error = "EAN inválido"
     
         if error:
@@ -156,7 +172,7 @@ def gerir_produtos_edit(sku):
                     existing_sku = cursor.execute("SELECT sku FROM product WHERE sku = %(sku)s;", {"sku": sku}).fetchone()
                     if not existing_sku:
                         error = "O SKU não existe"
-                    elif ean != "NULL":
+                    elif ean != None:
                         existing_ean = cursor.execute("SELECT ean FROM product WHERE ean = %(ean)s AND NOT sku = %(sku)s;", {"ean": ean, "sku": sku}).fetchone()
                         if existing_ean:
                             error = "O EAN já existe"
@@ -179,9 +195,9 @@ def gerir_produtos_edit(sku):
 def gerir_produtos_add():
     sku = request.form["sku"]
     name = request.form["name"]
-    description = request.form["description"] if request.form["description"] else "NULL"
+    description = request.form["description"] if request.form["description"] else None
     price = request.form["price"]
-    ean = request.form["ean"] if request.form["ean"] else "NULL"
+    ean = request.form["ean"] if request.form["ean"] else None
     error = ""
     
     if not sku:
@@ -190,7 +206,9 @@ def gerir_produtos_add():
         error = "Nome inválido"
     elif not price or (not price.isnumeric() and not re.fullmatch(float_regex, price)):
         error = "Preço inválido"
-    elif ean != "NULL" and not ean.isnumeric():
+    elif float(price) < 0:
+        error = "Preço inválido"
+    elif ean != None and not ean.isnumeric():
         error = "EAN inválido"
 
     if error:
@@ -202,7 +220,7 @@ def gerir_produtos_add():
                 existing_sku = cursor.execute("SELECT sku FROM product WHERE sku = %(sku)s;", {"sku": sku}).fetchone()
                 if existing_sku:
                     error = "O SKU já existe"
-                elif ean != "NULL":
+                elif ean != None:
                     existing_ean = cursor.execute("SELECT ean FROM product WHERE ean = %(ean)s;", {"ean": ean}).fetchone()
                     if existing_ean:
                         error = "O EAN já existe"
@@ -225,7 +243,7 @@ def gerir_fornecedores():
     with pool.connection() as conn:
         with conn.cursor() as cursor:
             cursor.execute("SELECT TIN, name FROM supplier;")
-            parser = lambda el : {"id": el[0], "name": el[1]}
+            parser = lambda el : {"id": el[0], "name": el[1] if el[1] != None else ""}
             suppliers = list(map(parser, cursor.fetchall()))
     return render_template("gerir-fornecedores.html", suppliers=suppliers)
 
@@ -242,8 +260,8 @@ def gerir_fornecedores_delete(tin):
 @app.route("/gerir-fornecedores/add", methods=("POST",))
 def gerir_fornecedores_add():
     tin = request.form["tin"]
-    name = request.form["name"] if request.form["name"] else "NULL"
-    address = request.form["address"] if request.form["address"] else "NULL"
+    name = request.form["name"] if request.form["name"] else None
+    address = request.form["address"] if request.form["address"] else None
     sku = request.form["sku"]
     error = ""
     
@@ -261,7 +279,7 @@ def gerir_fornecedores_add():
                 existing_tin = cursor.execute("SELECT tin FROM supplier WHERE tin = %(tin)s;", {"tin": tin}).fetchone()
                 if existing_tin:
                     error = "O TIN já existe"
-                existing_sku = cursor.execute("SELECT sku FROM supplier WHERE sku = %(sku)s;", {"sku": sku}).fetchone()
+                existing_sku = cursor.execute("SELECT sku FROM product WHERE sku = %(sku)s;", {"sku": sku}).fetchone()
                 if not existing_sku:
                     error = "O SKU não existe"
                 if error:
@@ -309,6 +327,9 @@ def gerir_encomendas_new():
         error = ""
         for key in request.form.keys():
             if key != "cust_no" and request.form[key] != "0":
+                if not request.form[key].isnumeric() or int(request.form[key]) < 0:
+                    error = "Quantidade inválida"
+                    break
                 products.append((key, request.form[key]))
     
         if not cust_no:
@@ -320,7 +341,7 @@ def gerir_encomendas_new():
             with pool.connection() as conn:
                 with conn.cursor() as cursor:
                     cursor.execute("START TRANSACTION;")
-                    existing_cust_no = cursor.execute("SELECT cust_no FROM customer WHERE cust_no = %(cust_no)s AND NOT email = 'deleted';", {"cust_no": cust_no}).fetchone()
+                    existing_cust_no = cursor.execute("SELECT cust_no FROM customer WHERE cust_no = %(cust_no)s AND NOT email LIKE '%deleted';", {"cust_no": cust_no}).fetchone()
                     if not existing_cust_no:
                         error = "O número de cliente não existe"
                     else:
@@ -375,12 +396,15 @@ def realizar_encomenda():
         with pool.connection() as conn:
             with conn.cursor() as cursor:
                 cursor.execute("START TRANSACTION;")
-                existing_cust_no = cursor.execute("SELECT cust_no FROM customer WHERE cust_no = %(cust_no)s AND NOT email = 'deleted';", {"cust_no": cust_no}).fetchone()
+                existing_cust_no = cursor.execute("SELECT cust_no FROM customer WHERE cust_no = %(cust_no)s AND NOT email LIKE '%deleted';", {"cust_no": cust_no}).fetchone()
                 if not existing_cust_no:
                     error = "O número de cliente não existe"
                 existing_order_no = cursor.execute("SELECT order_no FROM orders WHERE order_no = %(order_no)s;", {"order_no": order_no}).fetchone()
                 if not existing_order_no:
                     error = "O número de encomenda não existe"
+                ordered_by_client = cursor.execute("SELECT order_no FROM orders WHERE order_no = %(order_no)s AND cust_no = %(cust_no)s;", {"order_no": order_no, "cust_no": cust_no}).fetchone()
+                if not ordered_by_client:
+                    error = "A encomenda não foi feita por este cliente"
 
                 if error:
                     flash(error)
